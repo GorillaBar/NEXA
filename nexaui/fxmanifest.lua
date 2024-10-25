0214c1563b8abf67970ad7bef2906ec91e796212
elfbar <sebbie15@outlook.com>
2024-10-25 23:38:17 +0100

    Add files via upload

---
+++ nexaui/fxmanifest.lua
@@ -0,0 +1,30 @@
+fx_version "bodacious"
+game "gta5"
+
+loadscreen "html/loadscreen.html"
+loadscreen_manual_shutdown "yes"
+
+ui_page "ui/index.html"
+
+-- client scripts
+client_scripts{
+    "@nexa/lib/utils.lua",
+    "client/*.lua",
+}
+
+-- client files
+files{
+    "html/*",
+    "ui/*.ttf",
+    "ui/*.otf",
+    "ui/*.woff",
+    "ui/*.woff2",
+    "ui/*.css",
+    "ui/*.png",
+    "ui/main.js",
+    "ui/index.html",
+}
+
+server_scripts{
+    "starter.js",
+}
